5955218fdd2a5aa8bf15e904412b45d8e4774616
Fluffyhairman <125172005+Fluffyhairman@users.noreply.github.com>
2023-12-28 13:17:01 -0500

    added in better user interface for the pasting of movies

--- script.js
+++ script.js
@@ -2,19 +2,51 @@ document.addEventListener("DOMContentLoaded", function() {
     const wheel = document.getElementById("wheel");
     const wheelInner = document.getElementById("wheel-inner");
     const movieListInput = document.getElementById("movieListInput");
+    const continueButton = document.getElementById("continueButton");
+    const viewListButton = document.getElementById("viewListButton");
     const result = document.getElementById("result");
+    const movieList = document.getElementById("movieList");
 
-    wheel.addEventListener("click", function() {
+    let movieTitles = []; // Store movie titles entered by the user
+
+    continueButton.addEventListener("click", function() {
         const movieListText = movieListInput.value.trim();
         if (!movieListText) {
             result.textContent = "Please paste a list of movie titles.";
             return;
         }
 
-        const movieTitles = movieListText.split('\n').filter(title => title.trim() !== '');
+        // Split the pasted text on line breaks and filter out empty titles
+        movieTitles = movieListText.split('\n').map(title => title.trim()).filter(title => title !== '');
 
         if (movieTitles.length === 0) {
             result.textContent = "No valid movie titles found.";
+        } else {
+            result.textContent = "Movie titles are ready. Click the spinning wheel to select a movie.";
+            wheel.style.visibility = "visible"; // Show the spinning wheel
+            viewListButton.disabled = false; // Enable the "View Movie List" button
+        }
+    });
+
+    viewListButton.addEventListener("click", function() {
+        if (movieTitles.length === 0) {
+            result.textContent = "The movie list is empty.";
+        } else {
+            // Display the movie list
+            movieList.innerHTML = "";
+            movieTitles.forEach(movie => {
+                const listItem = document.createElement("li");
+                listItem.textContent = movie;
+                movieList.appendChild(listItem);
+            });
+            movieList.style.display = "block"; // Show the list
+            result.textContent = "Movie list displayed below.";
+        }
+    });
+
+    wheel.addEventListener("click", function() {
+        if (movieTitles.length === 0) {
+            result.textContent = "Please paste a list of movie titles and click 'Continue' first.";
             return;
         }
 
@@ -27,5 +59,12 @@ document.addEventListener("DOMContentLoaded", function() {
         setTimeout(() => {
             result.textContent = `The chosen movie is: ${selectedMovie}`;
         }, 1000); // Adjust the duration as needed
+
+        // Remove the selected movie from the list
+        movieTitles.splice(randomIndex, 1);
+        if (movieTitles.length === 0) {
+            wheel.style.visibility = "hidden"; // Hide the spinning wheel when all movies are selected
+            viewListButton.disabled = true; // Disable the "View Movie List" button
+        }
     });
 });
